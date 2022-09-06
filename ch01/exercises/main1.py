0ff3e9dfb1e7b21eeec86e4d4894017585ac7997
Richard Larson <112139726+Rlarson56@users.noreply.github.com>
2022-09-06 22:05:28 +0000

    working with data 2

--- ch01/exercises/main1.py
+++ ch01/exercises/main1.py
@@ -1,5 +1,5 @@
 print(10 * 5)
-print(10 * 2)
+print(10 ** 2)
 print(15 / 10)
 print(15 // 10)
 print(-15 // 10)
@@ -7,4 +7,11 @@ print(15 % 10)
 print(10 % 15)
 print(10 % 10)
 print(0 % 10)
-print(10 / 15) ##the solution creates an infinite response that is only limited by the computer, so you will never see the real answer
+print(10 / 15) ##the solution creates an infinite response that is only limited by the computer, so you will never see the full answer
+
+## part 2
+rate = int(input("What is the current exchange rate from Euro to Dollar? "))
+amount = int(input("How much would you like to exchange? "))
+total=rate*amount
+total=total-3
+print(total)
