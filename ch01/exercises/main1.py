c3fba008cc36df4b6f79608387ac4323bca1cea5
Richard Larson <112139726+Rlarson56@users.noreply.github.com>
2022-09-06 20:14:35 +0000

    working with data

---
+++ ch01/exercises/main1.py
@@ -0,0 +1,10 @@
+print(10 * 5)
+print(10 * 2)
+print(15 / 10)
+print(15 // 10)
+print(-15 // 10)
+print(15 % 10)
+print(10 % 15)
+print(10 % 10)
+print(0 % 10)
+print(10 / 15) ##the solution creates an infinite response that is only limited by the computer, so you will never see the real answer
