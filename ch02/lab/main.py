b1025a4a790f1dacb0dffff33ecc34266ea907ff
Richard Larson <112139726+Rlarson56@users.noreply.github.com>
2022-09-09 14:14:45 +0000

    ch02 lab

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,16 @@
-
+import random
 #Part A
 weeks = 16
-classes = 5
+print(weeks, "This is a numeric variable")
+classes = 3
+print(classes, "This is a numeric variable")
 tuition = 6000
+print(tuition, "This is a numeric variable")
 cost_per_week = ((tuition / classes) / weeks)
-print("Cost per week:", cost_per_week)
+print("The cost of my CS class is per week:", cost_per_week, "dollars")
 
 
 #Part B
+list=(1,2,3,4,5)
+rlist=random.choice(list)
+print(rlist)
