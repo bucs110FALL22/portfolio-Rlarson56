2d45792b20bfb8213cd15ccdcaf7b04f178b1dec
Richard Larson <112139726+Rlarson56@users.noreply.github.com>
2022-09-14 21:28:49 +0000

    ch02 drawing.py 2.0

--- ch02/exercises/drawing.py
+++ ch02/exercises/drawing.py
@@ -3,16 +3,17 @@ import turtle
 my_turtle=turtle.Turtle()
 
 my_turtle.shape("turtle")
-my_turtle.color("red")
+my_turtle.color("black")
 
 
 num_sides=input("How many sides would you like your shape to have..  ")
 num_sides=int(num_sides)
-legnth=30
+length=input("How long would you like each sides of your shape to be..  ")
+length=int(length)
 angle=360/num_sides
 angle=int(angle)
 for i in [angle]*num_sides:
-  my_turtle.forward(legnth)
+  my_turtle.forward(length)
   my_turtle.right(i)
 
 
