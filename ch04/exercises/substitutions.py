e07995a0127e19675e8d36e654fe2013cd3969ec
Richard Larson <112139726+Rlarson56@users.noreply.github.com>
2022-09-28 21:39:11 +0000

    substitutions

---
+++ ch04/exercises/substitutions.py
@@ -0,0 +1,12 @@
+premise= "Set in Albuquerque, New Mexico, between 2008 and 2010, Breaking Bad follows Walter White, a modest high school chemistry teacher who transforms into a ruthless kingpin in the local methamphetamine drug trade, driven to financially provide for his family after being diagnosed with inoperable lung cancer. Initially making only small batches of meth with his former student Jesse Pinkman in a rolling meth lab, Walter and Jesse eventually expand to make larger batches of special blue meth that is incredibly pure and creates high demand. Walter takes on the name Heisenberg to mask his identity."
+
+npremise=premise.replace("Albuquerque","that spot")
+lpremise=npremise.replace("Breaking Bad","the best to ever do it")
+opremise=lpremise.replace("Jesse","Cap n' Cook")
+ppremise=opremise.replace("Heisenberg","Heisenburger")
+qpremise=ppremise.replace("Walter","WALTA")
+rpremise=qpremise.replace("lung cancer", "taking action in his life")
+
+print(rpremise)
+
+ 
