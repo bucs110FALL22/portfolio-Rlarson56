ede98c88794ff147cb74dc7fb52c9c32f1163ce8
Richard Larson <112139726+Rlarson56@users.noreply.github.com>
2022-10-09 22:25:27 +0000

    coinflip

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -1,23 +1,27 @@
+## cd ch04/exercises
+## python3 coinflip.py
 import turtle
 import random
 
 a_turtle=turtle.Turtle()
 a_turtle.shape("turtle")
 a_turtle.color("black")
-a_turtle.home()
+a_turtle.setpos(0,0)
+window=turtle.Screen()
+turtle.screensize(canvwidth=500, canvheight=500, bg="white")
+canvwidth=turtle.window_width()
+canvheight=turtle.window_height()
 
-xc=a_turtle.xcor()
-yc=a_turtle.ycor()
-
-while 100>xc>-100 and 100>yc>-100:
- side==randrange(0,1) 
-  if side == 0: 
-    a_turtle.left(90)
-   elif side == 1:
-    a_turtle.right(90)
-  a_turtle.foward(20)
-
-
+while a_turtle.xcor() < 100 and  a_turtle.ycor()< 100:
+ side=random.randrange(1,3)
+ side=int(side)
+ if side == 1:
+     a_turtle.left(90)
+     print("HEADS")
+ else:
+     a_turtle.right(90)
+     print("TAILS")
+ a_turtle.forward(40)
 
 
 turtle.Screen().exitonclick()
