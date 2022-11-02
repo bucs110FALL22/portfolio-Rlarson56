affb047cd28d8ef6addc2ea808979f961f2121dc
Richard Larson <112139726+Rlarson56@users.noreply.github.com>
2022-11-02 22:48:50 +0000

    shltr

---
+++ ch07/exercises/shelter.py
@@ -0,0 +1,27 @@
+#cd ch07/exercises
+#python3 shelter.py
+import time
+
+class Animal:
+  def __init__(self, name, type):
+      self.name = name
+      self.type = type
+      self.id = time.strftime("%d%m%M%S")
+      self.arrived = time.strftime("%d/%m/%Y")
+      self.adopted = None
+
+  def set_adopted(self):
+      self.adopted = time.strftime("%d/%m/%Y")
+
+  def __str__(self):
+    result_str = f"{self.name}[{self.type}]"
+    result_str += f"\narrived: {self.arrived}"
+    result_str += f"\nadopted:{self.adopted}"
+    return result_str
+
+def main():
+  jovie = Animal("jovie" , "cat")
+  jovie.set_adopted()
+  print(jovie)
+main()
+   
